3c8b07dab4121bd0f5817aef377438b01edbcdd3
Mauro Sarti <mauro.sarti@globant.com>
2020-01-11 18:30:53 -0300

    Movies adapter complete
    Tested in: Samsung Galaxy S8
    Signed off by: Mauro Sarti

--- presentation/src/main/java/com/sartimau/moviesdb/adapters/MoviesAdapter.kt
+++ presentation/src/main/java/com/sartimau/moviesdb/adapters/MoviesAdapter.kt
@@ -3,6 +3,7 @@ package com.sartimau.moviesdb.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sartimau.domain.entities.MovieItem
 import com.sartimau.moviesdb.R
@@ -13,19 +14,58 @@ import kotlinx.android.synthetic.main.item_movie.view.movieImage
 import kotlinx.android.synthetic.main.item_movie.view.movieOverview
 import kotlinx.android.synthetic.main.item_movie.view.movieTitle
 
-class MoviesAdapter(private val movies: List<MovieItem> = emptyList(), private val listener: MovieListener) :
-    RecyclerView.Adapter<MoviesAdapterViewHolder>() {
+private const val MOVIE = 0
+private const val LOADING = 1
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesAdapterViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
-        return MoviesAdapterViewHolder(itemView, listener)
+class MoviesAdapter(private val movies: MutableList<MovieItem>, private val listener: MovieListener) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var isLoadingAdded = false
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val movieItemView = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        val loadingItemView = LayoutInflater.from(parent.context).inflate(R.layout.item_loading, parent, false)
+
+        return when (viewType) {
+            MOVIE -> MoviesAdapterViewHolder(movieItemView, listener)
+            LOADING -> LoadingViewHolder(loadingItemView)
+            else -> throw IllegalArgumentException("Wrong view type")
+        }
     }
 
-    override fun onBindViewHolder(holder: MoviesAdapterViewHolder, position: Int) {
-        holder.bind(movies[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            MOVIE -> (holder as MoviesAdapterViewHolder).bind(movies[position])
+            LOADING -> { } // do nothing
+        }
     }
 
     override fun getItemCount(): Int = movies.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == movies.size - 1 && isLoadingAdded) LOADING else MOVIE
+    }
+
+    fun addMovies(newMovies: List<MovieItem>) {
+        newMovies.forEach {
+            movies.add(it)
+            notifyItemInserted(movies.size - 1)
+        }
+    }
+
+    fun addLoadingFooter() {
+        isLoadingAdded = true
+        movies.add(MovieItem())
+        notifyItemInserted(movies.size - 1)
+    }
+
+    fun removeLoadingFooter() {
+        isLoadingAdded = false
+
+        val position = movies.size - 1
+        movies.removeAt(position)
+        notifyItemRemoved(position)
+    }
 }
 
 class MoviesAdapterViewHolder(view: View, val listener: MovieListener) : RecyclerView.ViewHolder(view) {
@@ -41,4 +81,35 @@ class MoviesAdapterViewHolder(view: View, val listener: MovieListener) : Recycle
     }
 }
 
+class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
 typealias MovieListener = (MovieItem) -> Unit
+
+abstract class PaginationScrollListener
+    (private var layoutManager: LinearLayoutManager) : RecyclerView.OnScrollListener() {
+
+    abstract val totalPageCount: Int
+
+    abstract val isLastPage: Boolean
+
+    abstract val isLoading: Boolean
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+
+        val visibleItemCount = layoutManager.childCount
+        val totalItemCount = layoutManager.itemCount
+        val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+        if (!isLoading && !isLastPage) {
+            if (visibleItemCount + firstVisibleItemPosition >= totalItemCount &&
+                firstVisibleItemPosition >= 0 &&
+                totalItemCount >= totalPageCount
+            ) {
+                loadMoreItems()
+            }
+        }
+    }
+
+    protected abstract fun loadMoreItems()
+}
